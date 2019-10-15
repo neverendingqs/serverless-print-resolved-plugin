0c5a6887ca4db3d827be47c29192e5387f3d801f
Mark Tse <neverendingqs@users.noreply.github.com>
2019-10-14 23:09:26 -0400

    plugin - now supports depth config. (#2)
    * plugin - now supports depth config.
    
    * chore - bump version.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const _ = {
+  get: require('lodash.get')
+};
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
@@ -14,8 +17,13 @@ class PrintResolved {
   }
 
   writeResolved() {
-    const stringified = util.inspect(this.serverless, { depth: 2 });
-    const filePath = path.join(this.serverless.serverless.config.servicePath, '.serverless', 'resolved-serverless.txt');
+    const { depth } = _.get(this.serverless, 'custom.print-resolved', {});
+    const stringified = util.inspect(this.serverless, { depth: depth || 2 });
+    const filePath = path.join(
+      this.serverless.serverless.config.servicePath,
+      '.serverless',
+      'resolved-serverless.txt'
+    );
     fs.writeFileSync(filePath, stringified, 'utf-8');
   }
 }
